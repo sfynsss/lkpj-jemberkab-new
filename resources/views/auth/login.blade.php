760a19a449db9ce8d91f36e4772b0a19aac3ab9a
Biyonda Kurniawan / LV <biyondakurniawan@gmail.com>
2022-11-03 06:52:11 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
+    <title>Login LKPJ Bappeda 2022</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
@@ -41,6 +41,7 @@
 
               <!-- From -->
               <form class="d-grid gap-3 my-5" action="{{ route('login') }}" method="POST">
+                @csrf
                 <div class="form-floating">
                   <input type="text" class="form-control rounded-0" name="username" value="{{ old('username') }}" id="username" placeholder="Masukkan username pengguna" required />
                   <label for="username"><span class="bi-person"></span> Username</label>
